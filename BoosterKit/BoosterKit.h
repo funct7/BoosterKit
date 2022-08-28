4a74c7bf19c27833cc785d21ebd5f56a72172f6c
Josh Park <funct7.io@gmail.com>
2022-08-28 13:26:07 +0900

    Initial Commit

---
+++ BoosterKit/BoosterKit.h
@@ -0,0 +1,18 @@
+//
+//  BoosterKit.h
+//  BoosterKit
+//
+//  Created by Josh Woomin Park on 2022/08/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BoosterKit.
+FOUNDATION_EXPORT double BoosterKitVersionNumber;
+
+//! Project version string for BoosterKit.
+FOUNDATION_EXPORT const unsigned char BoosterKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BoosterKit/PublicHeader.h>
+
+
