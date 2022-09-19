85421be7f75ceb8f34f6c81f2064ba24ec60fbf0
Josh Park <funct7.io@gmail.com>
2022-09-19 22:25:11 +0900

    [ui/calendar] Add: CalendarAdapter - UICollectionViewAdapter, LayoutPlan

---
+++ BoosterKit/UI/Calendar/CalendarAdapter.UICollectionViewAdapter.swift
@@ -0,0 +1,77 @@
+//
+//  CalendarAdapter.UICollectionViewAdapter.swift
+//  BoosterKit
+//
+//  Created by Josh Woomin Park on 2022/09/19.
+//
+
+import UIKit
+
+extension CalendarAdapter {
+    
+    class UICollectionViewAdapter : NSObject, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout where Cell : UICollectionViewCell {
+        
+        unowned let calendarAdapter: CalendarAdapter<Cell>
+
+        init(calendarAdapter: CalendarAdapter<Cell>) {
+            self.calendarAdapter = calendarAdapter
+        }
+        
+        private var _cache: [ISO8601Month : LayoutPlan] = [:]
+        
+        func numberOfSections(in collectionView: UICollectionView) -> Int { 3 }
+        
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            precondition((0...2).contains(section))
+            
+            let month = calendarAdapter.currentMonth.advanced(by: section - 1)
+            
+            if _cache[month] == nil { _cache[month] = .create(month: month) }
+            
+            return Int(_cache[month]!.numberOfDays)
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            calendarAdapter.viewProvider.getCell(
+                collectionView: collectionView,
+                for: _getLayoutContext(indexPath: indexPath))
+        }
+        
+        private func _getLayoutContext(indexPath: IndexPath) -> CalendarAdapterContext {
+            fatalError()
+        }
+        
+    }
+    
+}
+
+extension CalendarAdapter.UICollectionViewAdapter {
+    
+    struct LayoutPlan : Hashable {
+        let leadingDays: UInt
+        let numberOfDays: UInt
+        let numberOfWeeks: UInt
+        let trailingDays: UInt
+        
+        static func create(month: ISO8601Month) -> Self {
+            let cal = withVar(Calendar(identifier: .gregorian)) { $0.timeZone = month.timeZone }
+            let firstDay = ISO8601Date(date: month.instantRange.lowerBound, timeZone: month.timeZone)
+            let nextMonthFirstDay = ISO8601Date(date: month.instantRange.upperBound, timeZone: month.timeZone)
+            
+            let leadingDays = cal.dateComponents([.weekday], from: firstDay.instantRange.lowerBound).weekday! - 1
+            let numberOfDays = try! firstDay.distance(to: nextMonthFirstDay)
+            let numberOfWeeks = (leadingDays + numberOfDays + 7 - 1) / 7
+            let trailingDays: Int = assign {
+                let lastDay = nextMonthFirstDay.advanced(by: -1)
+                return 7 - cal.dateComponents([.weekday], from: lastDay.instantRange.lowerBound).weekday!
+            }
+            
+            return LayoutPlan(
+                leadingDays: UInt(leadingDays),
+                numberOfDays: UInt(numberOfDays),
+                numberOfWeeks: UInt(numberOfWeeks),
+                trailingDays: UInt(trailingDays))
+        }
+    }
+
+}
--- BoosterKit/UI/Calendar/CalendarAdapter.swift
+++ BoosterKit/UI/Calendar/CalendarAdapter.swift
@@ -24,7 +24,12 @@ public enum CalendarAdapterDisplayOption {
 // TODO: Generalize
 open class CalendarAdapter<Cell> where Cell : UICollectionViewCell {
     
-    @IBOutlet open weak var view: UICollectionView? = nil
+    @IBOutlet open weak var view: UICollectionView! = nil {
+        didSet {
+            view.dataSource = _adapter
+            view.delegate = _adapter
+        }
+    }
     open var viewProvider: AnyCalendarAdapterComponentViewProvider<Cell>
     open var delegate: AnyCalendarAdapterDelegate<Cell>? = nil
     open var displayOption: CalendarAdapterDisplayOption = .flexibleMonthHeight
@@ -34,10 +39,15 @@ open class CalendarAdapter<Cell> where Cell : UICollectionViewCell {
     open func getCell(date: ISO8601Date) -> Cell? { nil }
     open func scroll(to month: ISO8601Month) { }
     
+    open func reload() { }
+    open func reloadDate(_ date: ISO8601Date) { }
+    
     /// Elements are ordered by their selection order, not the natural order of `ISO8601Date`.
     open var selectedDates: [ISO8601Date] = []
     open func toggleSelection(date: ISO8601Date) {
     }
+    
+    private lazy var _adapter = UICollectionViewAdapter(calendarAdapter: self)
     
     /**
      - Parameters:
@@ -72,9 +82,36 @@ open class CalendarAdapter<Cell> where Cell : UICollectionViewCell {
 
 public protocol CalendarAdapterComponentViewProvider : AnyObject {
     associatedtype Cell : UICollectionViewCell
-    func cellForContext(_ context: CalendarAdapterContext) -> Cell
-    func decorationViewForWeekday(_ weekday: Weekday) -> UIView?
-    func headerViewForWeekday(_ weekday: Weekday) -> UIView?
+    func getCell(collectionView: UICollectionView, for context: CalendarAdapterContext) -> Cell
+    /**
+     A method to provide a **decoration view** to show a weekday at the top of the calendar.
+     
+     Return a view that presents the given weekday to have a fixed *decoration view* above the calendar;
+     i.e. the weekday row will be fixed, and won't be scrolled with the monthly calendar.
+     
+     To have weekdays scroll with the monthly calendar, i.e. a header, return `nil` from this method,
+     and return a valid view from `getHeaderView(collectionView:for:)` instead.
+     
+     - Warning: Returning a view for both this method **and** `getHeaderView(collectionView:for:)` will result in undefined behavior.
+     - Note: The position of the decoration view is at the top of the calendar. Report an issue if other uses cases need to be supported.
+     - Returns: A view that presents `weekday`.
+     */
+    func getDecorationView(collectionView: UICollectionView, for weekday: Weekday) -> UIView?
+    
+    /**
+     A method to provide a **header view** to show a weekday at the top of the calendar.
+     
+     Return a view that presents the given weekday to have a scrolling *header view* above the calendar;
+     i.e. the weekday row will be scrolled with the monthly calendar.
+     
+     To have weekdays fixed above monthly calendar, i.e. a decoration view, return `nil` from this method,
+     and return a valid view from `getDecorationView(collectionView:for:)` instead.
+     
+     - Warning: Returning a view for both this method **and** `getDecorationView(collectionView:for:)` will result in undefined behavior.
+     - Note: The position of the decoration view is at the top of the calendar. Report an issue if other uses cases need to be supported.
+     - Returns: A view that presents `weekday`.
+     */
+    func getHeaderView(collectionView: UICollectionView, for weekday: Weekday) -> UIView?
 }
 
 /**
@@ -82,23 +119,23 @@ public protocol CalendarAdapterComponentViewProvider : AnyObject {
  */
 open class AnyCalendarAdapterComponentViewProvider<Cell> : CalendarAdapterComponentViewProvider where Cell : UICollectionViewCell {
 
-    private let _cellForContext: (CalendarAdapterContext) -> Cell
-    open func cellForContext(_ context: CalendarAdapterContext) -> Cell { _cellForContext(context) }
+    private let _getCellForContext: (UICollectionView, CalendarAdapterContext) -> Cell
+    public func getCell(collectionView: UICollectionView, for context: CalendarAdapterContext) -> Cell { _getCellForContext(collectionView, context) }
     
-    private let _decorationViewForWeekday: (Weekday) -> UIView?
-    open func decorationViewForWeekday(_ weekday: Weekday) -> UIView? { _decorationViewForWeekday(weekday) }
+    private let _getDecorationViewForWeekday: (UICollectionView, Weekday) -> UIView?
+    public func getDecorationView(collectionView: UICollectionView, for weekday: Weekday) -> UIView? { _getDecorationViewForWeekday(collectionView, weekday) }
     
-    private let _headerViewForWeekday: (Weekday) -> UIView?
-    open func headerViewForWeekday(_ weekday: Weekday) -> UIView? { _headerViewForWeekday(weekday) }
+    private let _getHeaderViewForWeekday: (UICollectionView, Weekday) -> UIView?
+    public func getHeaderView(collectionView: UICollectionView, for weekday: Weekday) -> UIView? { _getHeaderViewForWeekday(collectionView, weekday) }
     
     /**
      - Attention: The constructed instance will hold an **unowned reference** to the `viewProvider`.
         It is up to the caller to make sure that `viewProvider` lives throughout the lifecycle of the created `AnyCalendarAdapterComponentViewProvider` instance.
      */
     public init<P>(_ viewProvider: P) where P : CalendarAdapterComponentViewProvider, P.Cell == Cell {
-        self._cellForContext = { [unowned viewProvider] in viewProvider.cellForContext($0) }
-        self._decorationViewForWeekday = { [unowned viewProvider] in viewProvider.decorationViewForWeekday($0) }
-        self._headerViewForWeekday = { [unowned viewProvider] in viewProvider.headerViewForWeekday($0) }
+        self._getCellForContext = { [unowned viewProvider] in viewProvider.getCell(collectionView: $0, for: $1) }
+        self._getDecorationViewForWeekday = { [unowned viewProvider] in viewProvider.getDecorationView(collectionView: $0, for: $1) }
+        self._getHeaderViewForWeekday = { [unowned viewProvider] in viewProvider.getHeaderView(collectionView: $0, for: $1) }
     }
 }
 
@@ -109,6 +146,12 @@ public protocol CalendarAdapterDelegate : AnyObject {
      */
     func calendarPresenter(_ presenter: CalendarAdapter<Cell>, willChangeMonthFrom oldValue: ISO8601Month, to newValue: ISO8601Month)
     func calendarPresenter(_ presenter: CalendarAdapter<Cell>, didChangeMonthFrom oldValue: ISO8601Month, to newValue: ISO8601Month)
+}
+
+private enum _CollectionViewSection {
+    static let prevMonth = 0
+    static let currentMonth = 1
+    static let nextMonth = 2
 }
 
 /**
@@ -132,4 +175,13 @@ open class AnyCalendarAdapterDelegate<Cell> : CalendarAdapterDelegate where Cell
     }
 }
 
-public struct CalendarAdapterContext { }
+public struct CalendarAdapterContext {
+    public enum Position {
+        case leading
+        case main
+        case trailing
+    }
+    
+    public let date: ISO8601Date
+    public let position: Position
+}
