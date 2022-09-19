545aa587970ff0c2609a5f6e3417c6ade4f64d1c
Josh Park <funct7.io@gmail.com>
2022-09-19 20:08:55 +0900

    [ui] Update: CalendarAdapter uses ISO8601Month (WIP)

--- BoosterKit/UI/Calendar/CalendarAdapter.swift
+++ BoosterKit/UI/Calendar/CalendarAdapter.swift
@@ -29,28 +29,43 @@ open class CalendarAdapter<Cell> where Cell : UICollectionViewCell {
     open var delegate: AnyCalendarAdapterDelegate<Cell>? = nil
     open var displayOption: CalendarAdapterDisplayOption = .flexibleMonthHeight
     
-    open var currentMonth: Month {
-        get { fatalError() }
-        set { }
-    }
-//    open var monthRange:
+    open var currentMonth: ISO8601Month
+    open var monthRange: Pair<ISO8601Month?, ISO8601Month?>
     open func getCell(date: ISO8601Date) -> Cell? { nil }
-    open func scroll(to month: Month) { }
+    open func scroll(to month: ISO8601Month) { }
     
     /// Elements are ordered by their selection order, not the natural order of `ISO8601Date`.
-    open var selectedDates: [ISO8601Date] {
-        get { fatalError() }
-        set { }
-    }
+    open var selectedDates: [ISO8601Date] = []
     open func toggleSelection(date: ISO8601Date) {
     }
     
     /**
+     - Parameters:
+        - initialMonth: The initial month to display.
+     
+            It is a programmer error to provide an `initialMonth` value that falls outside of `monthRange`.
+        - monthRange: The range of months to display. If both values are `nil`, the calendar will show from the infinite past to the inifinite future.
+     
+            `monthRange.first` will be considered to be the lower bound, and `monthRange.second` the upper bound.
+     - Precondition:
+        - If both values are provided for `monthRange`, `monthRange.first! <= monthRange.second`.!
+        - The `initialMonth` value must fall within the provided `monthRange`.
      - Attention: The constructed instance will hold an **unowned reference** to the `viewProvider`.
         It is up to the caller to make sure that `viewProvider` lives throughout the lifecycle of the created `CalendarAdapter` instance
         or provide a new `AnyCalendarAdapterComponentViewProvider` should the `viewProvider` be released somewhere if the lifecycle.
      */
-    public init<P>(viewProvider: P) where P : CalendarAdapterComponentViewProvider, P.Cell == Cell {
+    public init<P>(
+        initialMonth: ISO8601Month = .init(),
+        monthRange: Pair<ISO8601Month?, ISO8601Month?> = Pair((nil, nil)),
+        viewProvider: P)
+    where P : CalendarAdapterComponentViewProvider, P.Cell == Cell
+    {
+        precondition(monthRange.first == nil || monthRange.second == nil || monthRange.first! <= monthRange.second!)
+        precondition(monthRange.first != nil ? monthRange.first! <= initialMonth : true)
+        precondition(monthRange.second != nil ? initialMonth <= monthRange.second! : true)
+        
+        self.currentMonth = initialMonth
+        self.monthRange = monthRange
         self.viewProvider = .init(viewProvider)
     }
 }
@@ -92,8 +107,8 @@ public protocol CalendarAdapterDelegate : AnyObject {
     /**
      This method is called immediately when the user stops dragging and it is determined which month the calendar will animate to.
      */
-    func calendarPresenter(_ presenter: CalendarAdapter<Cell>, willChangeMonthFrom oldValue: Month, to newValue: Month)
-    func calendarPresenter(_ presenter: CalendarAdapter<Cell>, didChangeMonthFrom oldValue: Month, to newValue: Month)
+    func calendarPresenter(_ presenter: CalendarAdapter<Cell>, willChangeMonthFrom oldValue: ISO8601Month, to newValue: ISO8601Month)
+    func calendarPresenter(_ presenter: CalendarAdapter<Cell>, didChangeMonthFrom oldValue: ISO8601Month, to newValue: ISO8601Month)
 }
 
 /**
@@ -101,11 +116,11 @@ public protocol CalendarAdapterDelegate : AnyObject {
  */
 open class AnyCalendarAdapterDelegate<Cell> : CalendarAdapterDelegate where Cell : UICollectionViewCell {
     
-    private let _willChangeMonth: (CalendarAdapter<Cell>, Month, Month) -> Void
-    open func calendarPresenter(_ presenter: CalendarAdapter<Cell>, willChangeMonthFrom oldValue: Month, to newValue: Month) { _willChangeMonth(presenter, oldValue, newValue) }
+    private let _willChangeMonth: (CalendarAdapter<Cell>, ISO8601Month, ISO8601Month) -> Void
+    open func calendarPresenter(_ presenter: CalendarAdapter<Cell>, willChangeMonthFrom oldValue: ISO8601Month, to newValue: ISO8601Month) { _willChangeMonth(presenter, oldValue, newValue) }
     
-    private let _didChangeMonth: (CalendarAdapter<Cell>, Month, Month) -> Void
-    open func calendarPresenter(_ presenter: CalendarAdapter<Cell>, didChangeMonthFrom oldValue: Month, to newValue: Month) { _didChangeMonth(presenter, oldValue, newValue) }
+    private let _didChangeMonth: (CalendarAdapter<Cell>, ISO8601Month, ISO8601Month) -> Void
+    open func calendarPresenter(_ presenter: CalendarAdapter<Cell>, didChangeMonthFrom oldValue: ISO8601Month, to newValue: ISO8601Month) { _didChangeMonth(presenter, oldValue, newValue) }
     
     /**
      - Note: The constructed `AnyCalendarAdapterDelegate` instance will **NOT** hold a strong reference to the provided `delegate`.
@@ -116,7 +131,5 @@ open class AnyCalendarAdapterDelegate<Cell> : CalendarAdapterDelegate where Cell
         self._didChangeMonth = { [weak delegate] in delegate?.calendarPresenter($0, didChangeMonthFrom: $1, to: $2) }
     }
 }
-
-public struct Month : Equatable { }
 
 public struct CalendarAdapterContext { }
