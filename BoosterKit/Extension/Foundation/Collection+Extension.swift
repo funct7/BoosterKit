cf97e1fe7ac57de4019fd8d4dd2a2dc01aca9b1f
Josh Park <funct7.io@gmail.com>
2022-10-16 12:29:44 +0900

    [ext] Add: Collection.isNotEmpty

---
+++ BoosterKit/Extension/Foundation/Collection+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Collection+Extension.swift
+//  BoosterKit
+//
+//  Created by Josh Woomin Park on 2022/10/16.
+//
+
+import Foundation
+
+public extension Collection {
+    
+    var isNotEmpty: Bool { !isEmpty }
+    
+}
