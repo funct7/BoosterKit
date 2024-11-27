dd8de4ebb1e68626fc40dd74350568e14c528d4e
Josh Park <funct7.io@gmail.com>
2024-11-28 06:13:25 +0900

    [lib] U: scoped fn to internal access

--- BoosterKit/Function/Scoped.swift
+++ BoosterKit/Function/Scoped.swift
@@ -7,29 +7,29 @@
 
 import Foundation
 
-public func assign<T>(_ block: () -> T) -> T { block() }
+func assign<T>(_ block: () -> T) -> T { block() }
 
-public func assign<T>(_ block: () throws -> T) throws -> T { try block() }
+func assign<T>(_ block: () throws -> T) throws -> T { try block() }
 
-public func assign<T>(_ reader: ((T) -> Void) -> Void) -> T {
+func assign<T>(_ reader: ((T) -> Void) -> Void) -> T {
     var result: T!
     reader({ result = $0 })
     return result
 }
 
-public func withVar<T>(_ value: T, then: (inout T) -> Void) -> T {
+func withVar<T>(_ value: T, then: (inout T) -> Void) -> T {
     var value = value
     then(&value)
     return value
 }
 
-public func withVar<T, A>(_ value: T, _ keyPath: KeyPath<T, A>, then: (inout T, A) -> Void) -> T {
+func withVar<T, A>(_ value: T, _ keyPath: KeyPath<T, A>, then: (inout T, A) -> Void) -> T {
     var value = value
     then(&value, value[keyPath: keyPath])
     return value
 }
 
-public func withVar<T, A, B>(_ value: T, _ keyPath1: KeyPath<T, A>, _ keyPath2: KeyPath<T, B>, then: (inout T, A, B) -> Void) -> T {
+func withVar<T, A, B>(_ value: T, _ keyPath1: KeyPath<T, A>, _ keyPath2: KeyPath<T, B>, then: (inout T, A, B) -> Void) -> T {
     var value = value
     then(&value, value[keyPath: keyPath1], value[keyPath: keyPath2])
     return value
